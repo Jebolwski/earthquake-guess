f26fe52d5a929c9025e1a9be344bdd6bb18045f6
jebolwski <mertgkmeen@gmail.com>
2025-03-15 17:10:14 +0300

    font

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,38 +8,42 @@ const Login = () => {
   const { login }: any = useContext(AuthContext);
 
   return (
-    <div>
-      <p>{username}</p>
-      <p>{password}</p>
-      <div className="p-3">
-        <div>
-          <p>Username</p>
-          <input
-            type="text"
-            onChange={(e) => {
-              setUsername(e.target.value);
+    <div className="bg-amber-50 font-opensans h-full flex justify-center items-center p-4">
+      <div className="flex items-center bg-amber-100 w-fiy border border-amber-200 shadow-md rounded-lg">
+        <div className="p-3">
+          <p className="text-lg">
+            <label className="text-xl font-semibold">Login</label> to see your
+            building
+          </p>
+          <div className="mt-4">
+            <p className="text-sm">Username</p>
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="focus:outline-0 focus:shadow-lg w-full shadow-md duration-200 px-2 py-1 rounded-md bg-white border border-amber-300"
+            />
+          </div>
+          <div className="mt-4">
+            <p className="text-sm mt-2">Password</p>
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="focus:outline-0 focus:shadow-lg w-full shadow-md duration-200 px-2 py-1 rounded-md bg-white border border-amber-300"
+            />
+          </div>
+          <button
+            className="bg-yellow-500 mt-4 cursor-pointer text-white hover:shadow-lg shadow-md duration-200 px-4 rounded-md border border-amber-300 font-semibold py-1 px-2 mt-2"
+            onClick={() => {
+              login({ username, password });
             }}
-            className="border border-black"
-          />
+          >
+            Login
+          </button>
         </div>
-        <div>
-          <p>Password</p>
-          <input
-            type="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            className="border border-black"
-          />
-        </div>
-        <button
-          className="bg-red-400 px-2 mt-2"
-          onClick={() => {
-            login({ username, password });
-          }}
-        >
-          Login
-        </button>
       </div>
     </div>
   );
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -8,38 +8,42 @@ const Register = () => {
   const { register }: any = useContext(AuthContext);
 
   return (
-    <div>
-      <p>{username}</p>
-      <p>{password}</p>
-      <div className="p-3">
-        <div>
-          <p>Username</p>
-          <input
-            type="text"
-            onChange={(e) => {
-              setUsername(e.target.value);
+    <div className="bg-amber-50 font-opensans h-full flex justify-center items-center p-4">
+      <div className="flex items-center bg-amber-100  border border-amber-200 shadow-md rounded-lg w-fit">
+        <div className="p-3">
+          <p className="text-lg">
+            <label className="text-xl font-semibold">Register</label> to see
+            your building
+          </p>
+          <div className="mt-4">
+            <p className="text-sm">Username</p>
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="focus:outline-0 focus:shadow-lg w-full shadow-md duration-200 px-2 py-1 rounded-md bg-white border border-amber-300"
+            />
+          </div>
+          <div className="mt-4">
+            <p className="text-sm mt-2">Password</p>
+            <input
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="focus:outline-0 focus:shadow-lg w-full shadow-md duration-200 px-2 py-1 rounded-md bg-white border border-amber-300"
+            />
+          </div>
+          <button
+            className="bg-yellow-500 mt-4 cursor-pointer text-white hover:shadow-lg shadow-md duration-200 px-4 rounded-md border border-amber-300 font-semibold py-1 px-2 mt-2"
+            onClick={() => {
+              register({ username, password });
             }}
-            className="border border-black"
-          />
+          >
+            Register
+          </button>
         </div>
-        <div>
-          <p>Password</p>
-          <input
-            type="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            className="border border-black"
-          />
-        </div>
-        <button
-          className="bg-red-400 px-2 mt-2"
-          onClick={() => {
-            register({ username, password });
-          }}
-        >
-          Register
-        </button>
       </div>
     </div>
   );
